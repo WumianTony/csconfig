b447be6a9f9f0717ae3c8ed53d45cfda2b5bc79f
BuildTools <unconfigured@null.spigotmc.org>
2023-07-04 21:24:35 +0800

    fix(Binary): fixed the copy script of release bin

--- compile.bat
+++ compile.bat
@@ -1,9 +1,10 @@
 @echo off
-cd ./build >nul 2>&1
+cd .\build >nul 2>&1
 cmake -G "MinGW Makefiles" .. >nul 2>&1
 make
 if %errorlevel% equ 0 (
-    copy /V /Y csconfig.exe ../release/csconfig.exe >nul 2>&1
+    del ..\release\csconfig.exe
+    copy /B/V/Y csconfig.exe ..\release\csconfig.exe >nul 2>&1
 ) else (
     cd .. >nul 2>&1
 )
